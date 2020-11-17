93a5c3ff9e798a14e058769a33be12a0942d40c1
marie-brt <71490717+marie-brt@users.noreply.github.com>
2020-11-17 15:24:33 +0100

    Listes maisons
    - Ajout d'1 liste par maison
    - Génération random d'entre 5 et 10 voeu par type d'objet et par maison

--- Assets/Scripts/ListesMaisons.cs
+++ Assets/Scripts/ListesMaisons.cs
@@ -1,26 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+using System;
+using UnityEngine.UI;
 
 public class ListesMaisons : MonoBehaviour
 {
-    public GameObject[] mapetiteListe;
-    string afficheListe = "";
+    public Text ListedeVoeux;
+    public string[] mapetiteListe;
+    //string afficheListe = "";
+    //public transform = Livre;
 
 
     void Start()
     {
+        int VoeuLivre = Random.Range(5, 10);
+        int VoeuJeu = Random.Range(5, 10);
+        int VoeuPeluche = Random.Range(5, 10);
+        mapetiteListe[0] = "- " + VoeuLivre.ToString() + " livres\n";
+        mapetiteListe[1] = "- " + VoeuJeu.ToString() + " jeux" + "\n";
+        mapetiteListe[2] = "- " + VoeuPeluche.ToString() + " peluches";
+
+        print(mapetiteListe);
+        ListedeVoeux.text = mapetiteListe[0] + mapetiteListe[1] + mapetiteListe[2];
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        afficheListe = "";
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            afficheListe = afficheListe + "- " + transform.GetChild(i).name;
-        }
     }
-} 
+}
