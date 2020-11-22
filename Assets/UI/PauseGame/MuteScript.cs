66692e20cc8429a7e1fb2a56a3517030d8b869df
ayaben <ayalaazizi@gmail.com>
2020-11-22 14:18:15 +0100

    timer UI Marion

---
+++ Assets/UI/PauseGame/MuteScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteScript : MonoBehaviour
+{
+    public Button BoutonMute;
+    public Sprite SoundOFF, SoundON;
+    public bool Muted = false;
+
+    AudioSource[] sources;
+
+
+    void Start()
+    {
+        BoutonMute.GetComponent<Button>();
+        sources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+    }
+
+
+
+    public void MuteMusic()
+    {
+        if (Muted)
+        {
+            BoutonMute.image.overrideSprite = SoundOFF;
+            Mute(true);
+            Muted = false;
+        }
+        else
+        {
+            BoutonMute.image.overrideSprite = SoundON;
+            Mute(false);
+            Muted = true;
+        }
+    }
+
+    void Mute (bool sons)
+    {
+        foreach(AudioSource tousSons in sources)
+        {
+            tousSons.volume = sons ? 0 : 1 ;
+        }
+    }
+
+}
---
+++ Assets/UI/TimerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField]
+    int startTimer = 180;
+
+    [SerializeField]
+    Text TexteTimer;
+    void Start()
+    {
+        startTimer = startTimer + (int) Time.time;
+        // TexteTimer.text = "Temps restant  :  " + startTimer; 
+        //IEnumerator GameOver() { yield return new WaitForSeconds(startTimer = 0);SceneManager.GetSceneByName("GAMEOVER"); StartCoroutine(GameOver());}
+    }
+    private void Update()
+    {
+            float tempsRestant = startTimer - Time.time;
+            print((tempsRestant / 60).ToString());
+        TexteTimer.text = "Temps restant  :  " + ((int)(tempsRestant / 60)).ToString()
+            + ":" + ((int)(tempsRestant - ((int)(tempsRestant / 60)) * 60)).ToString();
+    }
+
+}
