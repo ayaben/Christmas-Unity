aea4f9be77540929511d66e56a6a3b0c8e910e31
marie-brt <71490717+marie-brt@users.noreply.github.com>
2020-11-29 13:55:05 +0100

    Ajout des commentaires dans le script PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Gestion de la vitesse du player
-    public float vitesse;
-    public float vitesseInitiale;
-    public float vitesseMax;
+    public float vitesse; //la vitesse du joueur qui prendra selon la situation la valeur de vitesseInitiale ou de vitesseMax
+    public float vitesseInitiale; //la vitesse normale du joueur
+    public float vitesseMax; //la vitesse maximale du joueur, qu'il pourra prendre lorsqu'il aura un bonus
     public float dureeAcceleration = 5f;
     float tempsAcceleration = 0;
 
@@ -24,29 +24,29 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!monScriptPause.Paused) // Si le bouton pause est enfoncé, le player ne bouge pas
         {
-            tempsAcceleration += Time.deltaTime;
+            tempsAcceleration += Time.deltaTime; //on ajoute à chaque Update le temps qui s'est écoulé en seconde depuis la dernière frame
 
             if (Input.GetKey(KeyCode.LeftArrow))
             {
-                transform.Translate(-vitesse, 0, 0);
+                transform.Translate(-vitesse, 0, 0); //si le joueur enfonce la touche gauche de son clavier, il se déplace vers la gauche
             }
 
             if (Input.GetKey(KeyCode.RightArrow))
             {
-                transform.Translate(vitesse, 0, 0);
+                transform.Translate(vitesse, 0, 0); //si le joueur enfonce la touche droite de son clavier, il se déplace vers la droite
             }
 
             if (Input.GetKey(KeyCode.DownArrow))
             {
-                transform.Translate(0, -vitesse, 0);
+                transform.Translate(0, -vitesse, 0); //si le joueur enfonce la touche avec la flèche vers le haut, il va vers le haut
             }
 
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                transform.Translate(0, vitesse, 0);
+                transform.Translate(0, vitesse, 0); //si le joueur enfonce la touche avec la flèche vers le bas, il va vers le bas
             }
 
-            if (vitesse == vitesseMax)
+            if (vitesse == vitesseMax) //si la vitesse est fixée à sa valeur maximale, donc si on a pris un sucre d'orge, on appelle la fonction UpdateVitesse pour rendre au joueur sa vitesse initiale une fois le temps du bonus terminé
             {
                 UpdateVitesse();
             }
@@ -56,20 +56,20 @@ public class PlayerMovement : MonoBehaviour
 
 
 
-    public void Acceleration()
+    public void Acceleration() //elle est appelée dans le script "GestionCollision" lors d'une collision avec le sucre d'orge
     {
-        vitesse = vitesseMax;
-        tempsAcceleration = 0;
+        vitesse = vitesseMax; //le joueur accélère, sa vitesse devient sa vitesse max
+        tempsAcceleration = 0; //on lui donne la valeur 0 au moment où le joueur commence à accélére. Il est modifié ensuite dans chaque Update
         // Accélération du fond musical 
-        musique.outputAudioMixerGroup.audioMixer.SetFloat("musiquePitch", 1.3f);
+        musique.outputAudioMixerGroup.audioMixer.SetFloat("musiquePitch", 1.3f); //on accélère aussi la musique
         musique.outputAudioMixerGroup.audioMixer.SetFloat("pitchShifter", 0.7f);
     }
 
     void UpdateVitesse()
     {
-        if (tempsAcceleration >= dureeAcceleration)
+        if (tempsAcceleration >= dureeAcceleration) //si le temps qui s'est écoulé depuis le début de l'accélération est supérieur ou égal à la durée maximale que l'on a définie pour cette accélération
         {
-            vitesse = vitesseInitiale;
+            vitesse = vitesseInitiale; //on rend au joueur sa vitesse initiale
             // Reprise du fond musical à vitesse normale
             musique.outputAudioMixerGroup.audioMixer.SetFloat("musiquePitch", 1f);
             musique.outputAudioMixerGroup.audioMixer.SetFloat("pitchShifter", 1f);
